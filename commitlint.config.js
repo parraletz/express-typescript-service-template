997f4e84027b8837f2376cab486a893c7acb7273
Alex Parra <parraletz@gmail.com>
2025-03-25 13:45:29 +0000

    fix: fix commit conventional

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,6 +12,7 @@ module.exports = {
     'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 120]
   }
 }
